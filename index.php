d5efc9cfba1252b5d9e947c5a0d12af5188176fc
Stanislav Bayer <stannik@seznam.cz>
2016-10-28 13:02:37 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="cs-cz">
+
+        <head>
+                <meta charset="utf-8" />
+                <link rel="stylesheet" href="styl.css" type="text/css" />
+                <title>Gynekologická ambulance</title>
+        </head>
+
+        <body>
+                <header>
+                        <div id="logo"><h1>HoBi</h1></div>
+                        <nav>
+                                <ul>
+                                        <li><a href="index.php?stranka=ordinace">Ordinace</a></li>
+                                        <li><a href="index.php?stranka=kontakt">Kontakt</a></li>
+                                </ul>
+                        </nav>
+                </header>
+
+                <article>
+                        <div id="centrovac">
+                                <header>
+                                        <h1>Ordinace</h1>
+                                </header>
+
+                                <section>
+                                        <?php
+
+                                        ?>
+                                </section>
+                                <div class="cistic"></div>
+                        </div>
+                </article>
+
+
+                <footer>
+                        Mendlovo náměstí, Brno
+                </footer>
+        </body>
+</html>
